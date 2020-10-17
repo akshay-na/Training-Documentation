8bc1f0465b3809354a007ffffbe3464d8086f145
Akshay N A <akshayna04698@gmail.com>
2020-10-17 19:37:57 +0530

    Sat, Oct 17, 2020  7:37:57 PM

--- git_push.sh
+++ git_push.sh
@@ -1,15 +1,16 @@
 #! /bin/sh
 
 RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 git add .
-echo -e "${RED}Staging Completed."
+echo -e "${RED}Staging Completed.${NC}"
 echo ""
 
 git commit -m "`date`"
 
-echo -e "${RED}Committed to Local Repo."
+echo -e "${RED}Committed to Local Repo.${NC}"
 echo ""
 #git push -u origin master
 
-echo -e "${RED}Pushed to Remote Repo."
+echo -e "${RED}Pushed to Remote Repo.${NC}"
