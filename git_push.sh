5650ce2da676b6fd18670045dee64a394173c102
Akshay N A <akshayna04698@gmail.com>
2020-10-17 20:12:46 +0530

    Sat, Oct 17, 2020  8:12:46 PM

--- git_push.sh
+++ git_push.sh
@@ -9,18 +9,18 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 git add .
-echo -e "${RED}Staging Completed.${NC}"
+echo -e "1) ${RED}Staging Completed.${NC}"
 echo ""
 
 git commit -m "`date`"
 
-echo -e "${RED}Committed to Local Repo.${NC}"
+echo -e "2) ${RED}Committed to Local Repo.${NC}"
 echo ""
 
 git remote add origin $REPO 2> /dev/null
 
-echo -e "${RED}Pushing to Remote Repo:${NC}${GREEN} $REPO ${NC}"
+echo -e "3) ${RED}Pushing to Remote Repo:${NC}${GREEN} $REPO ${NC}"
 echo ""
 #git push -u origin master
 
-echo -e "${RED}Pushed to Remote Repo.${NC}"
+echo -e "4) ${RED}Pushed to Remote Repo.${NC}"
